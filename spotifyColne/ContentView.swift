b5040f6facf0c8245142c7dc01390168758f13d0
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2022-05-22 19:22:36 +0530

    Initial Commit

---
+++ spotifyColne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spotifyColne
+//
+//  Created by Ketan Aggarwal on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spotifyColne/spotifyColneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyColneApp.swift
+//  spotifyColne
+//
+//  Created by Ketan Aggarwal on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct spotifyColneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
