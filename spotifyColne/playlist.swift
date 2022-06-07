28ad736c3667f89ce8eaf919e04748facc239659
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2022-06-07 14:45:26 +0530

    initial commit

---
+++ spotifyColne/playlist.swift
@@ -0,0 +1,20 @@
+//
+//  playlist.swift
+//  spotifyColne
+//
+//  Created by Ketan Aggarwal on 04/06/22.
+//
+
+import SwiftUI
+
+struct playlist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct playlist_Previews: PreviewProvider {
+    static var previews: some View {
+        playlist()
+    }
+}
