85e21f27c134c3ba3f29f9ff71ed6490bf61cec3
Ketan Aggarwal <64477717+ketanAggarwal58@users.noreply.github.com>
2022-06-07 14:45:33 +0530

    initial commit

--- spotifyColne/ContentView.swift
+++ spotifyColne/ContentView.swift
@@ -7,15 +7,76 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+extension Color {
+  init(_ hex: UInt, alpha: Double = 1) {
+    self.init(
+      .sRGB,
+      red: Double((hex >> 16) & 0xFF) / 255,
+      green: Double((hex >> 8) & 0xFF) / 255,
+      blue: Double(hex & 0xFF) / 255,
+      opacity: alpha
+    )
+  }
 }
 
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .topLeading){
+            LinearGradient(gradient: Gradient(colors: [Color(0xFFe67e22, alpha: 0.8), Color(0xFF191919), Color(0xFF191919), Color(0xFF191919), Color(0xFF191919), Color(0xFF191919), Color(0xFF191919)]), startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
+            VStack(){
+                HStack(){
+                    Text("Good Morning").font(.system(size: 25.0)).foregroundColor(.white).fontWeight(.bold).padding()
+                    Spacer()
+                    Image(systemName: "bell").foregroundColor(.white).padding([.bottom, .trailing, .top], 10).font(.system(size: 20.0))
+                    Image(systemName: "clock").foregroundColor(.white).padding([.bottom, .trailing, .top], 10).font(.system(size: 20.0))
+                    Image(systemName: "gear").foregroundColor(.white).padding([.bottom, .trailing, .top], 10).font(.system(size: 20.0))
+                }.padding()
+                VStack(){
+                    HStack(){
+                        playlist(playlistName: "Play List 1", playlistImage:
+                                    "house.fill", fontSize: 15.0, iconSize: 25).frame(width: 180, height: 60)
+                        Spacer()
+                        playlist(playlistName: "On Repeat", playlistImage:
+                                    "repeat.circle", fontSize: 15.0, iconSize: 25).frame(width: 180, height: 60)
+                        
+                    }
+                    HStack(){
+                        playlist(playlistName: "Liked Songs", playlistImage:
+                                    "heart.fill", fontSize: 15.0, iconSize: 25).frame(width: 180, height: 60)
+                        Spacer()
+                        playlist(playlistName: "Play List 4", playlistImage:
+                                    "repeat.circle", fontSize: 15.0, iconSize: 25).frame(width: 180, height: 60)
+                        
+                    }
+                    HStack(){
+                        playlist(playlistName: "Play List 5", playlistImage:
+                                    "house.fill", fontSize: 15.0, iconSize: 25).frame(width: 180, height: 60)
+                        Spacer()
+                        playlist(playlistName: "Play List 6", playlistImage:
+                                    "repeat.circle", fontSize: 15.0, iconSize: 25).frame(width: 180, height: 60)
+                        
+                    }
+                }.padding()
+                Text("Your top mixes").font(.system(size: 21.0)).fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .leading).padding()
+                Spacer()
+                HStack(){
+                    Spacer()
+                    Image(systemName: "house.fill").foregroundColor(.white).font(.system(size: 23.0))
+                    Spacer()
+                    Image(systemName: "magnifyingglass").foregroundColor(.white).font(.system(size: 23.0))
+                    Spacer()
+                    Image(systemName: "headphones").foregroundColor(.white).font(.system(size: 23.0))
+                    Spacer()
+                    Image(systemName: "person.circle").foregroundColor(.white).font(.system(size: 23.0))
+                    Spacer()
+                }.padding()
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
--- spotifyColne/playlist.swift
+++ spotifyColne/playlist.swift
@@ -7,14 +7,43 @@
 
 import SwiftUI
 
+//extension Color {
+//  init(_ hex: UInt, alpha: Double = 1) {
+//    self.init(
+//      .sRGB,
+//      red: Double((hex >> 16) & 0xFF) / 255,
+//      green: Double((hex >> 8) & 0xFF) / 255,
+//      blue: Double(hex & 0xFF) / 255,
+//      opacity: alpha
+//    )
+//  }
+//}
+
 struct playlist: View {
+    
+    var playlistName: String
+    var playlistImage: String
+    var fontSize: Double
+    var iconSize: Double
+//    var content: () -> Content
+    
+    init(playlistName: String, playlistImage: String, fontSize: Double, iconSize: Double){
+        self.playlistImage = playlistImage
+        self.playlistName = playlistName
+        self.fontSize = fontSize
+        self.iconSize = iconSize
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(){
+            Image(systemName: playlistImage).font(.system(size: iconSize)).frame(width: 50, height: 50, alignment: .center).foregroundColor(.white)
+            Text(playlistName).font(.system(size: fontSize)).foregroundColor(.white).padding()
+        }.overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(0xFF1c1c1c), lineWidth: 1)).background(LinearGradient(gradient: Gradient(colors: [Color(0xFFfffff, alpha: 0.4), Color(0xFF191919, alpha: 0.4)]), startPoint: .topLeading, endPoint: .bottomLeading))
     }
 }
 
 struct playlist_Previews: PreviewProvider {
     static var previews: some View {
-        playlist()
+        playlist(playlistName: "On Repeat", playlistImage: "house.fill", fontSize: 10.0, iconSize: 20.0)
     }
 }
